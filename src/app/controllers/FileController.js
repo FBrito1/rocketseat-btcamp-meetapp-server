d22f4d7be1d222f30766ff58430b65f00f12df0a
Fbrito1 <luan.tecno@gmail.com>
2019-09-07 19:11:26 -0300

    add dotenv config | upload files setup

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,11 @@
+import File from '../models/File';
+
+class FileController {
+  async store(req, res) {
+    return res.json({
+      ok: true,
+    });
+  }
+}
+
+export default new FileController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,5 +16,7 @@ routes.use(authMiddleware);
 routes.get('/users/:userId', UserController.index);
 routes.put('/users', UserController.update);
 routes.delete('/users/:userId', UserController.delete);
+
+routes.post('/files', FileController.store);
 
 export default routes;
